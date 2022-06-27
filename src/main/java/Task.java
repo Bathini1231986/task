615bc4c4d178faceaf930a5706ec605b76d1a4f3
nurkult <temirlan.nurkulov@dominos.com>
2022-06-27 14:08:48 -0500

    task

---
+++ src/main/java/Task.java
@@ -0,0 +1,22 @@
+public class Task {
+
+
+    public void task1(){
+        /**
+         * Count duplicate chars in given string
+         */
+    }
+    public void task2(){
+        /**
+         * swap two integers without using third variable
+         */
+    }
+
+    public void task3(){
+        /**
+         * swap two integers without using third variable
+         */
+    }
+
+
+}
---
+++ src/test/java/Task1.java
@@ -0,0 +1,25 @@
+import org.junit.Test;
+
+public class Task1 {
+
+    @Test
+    public void test(){
+        /**
+         * Count duplicate chars in given string
+         */
+    }
+
+    @Test
+    public void test2(){
+        /**
+         * swap two integers without using third variable
+         */
+    }
+
+    @Test
+    public void test3(){
+        /**
+         * revert string using while loop
+         */
+    }
+}
